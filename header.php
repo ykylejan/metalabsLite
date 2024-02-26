487839adad06f8762bc258a51697a4f1a1003277
r.montebon.536963@umindanao.edu.ph <ranielmontebon24@gmail.com>
2024-02-26 14:27:48 +0800

    tailwind scks

--- header.php
+++ header.php
@@ -13,22 +13,25 @@
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="tailwind.config.js"></script>
 </head>
 
 <body>
-    <nav x-data="{ isOpen: false }" class="relative bg-white shadow dark:bg-gray-800">
+    <nav x-data="{ isOpen: false }" class="relative bg-white shadow ">
         <div class="container px-6 py-1 mx-auto md:flex md:justify-between md:items-center">
             <div class="flex items-center justify-between">
-                <a href="#" class="font-extrabold text-2xl font-montserrat ">
-                    metaLABS LITE
+                <a href="#">
+                    <img class="w-auto h-6 sm:h-7" src="https://merakiui.com/images/full-logo.svg" alt="">
                 </a>
 
                 <!-- Mobile menu button -->
                 <div class="flex lg:hidden">
                     <button x-cloak @click="isOpen = !isOpen" type="button"
-                        class="text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
+                        class="text-gray-500  hover:text-gray-600  focus:outline-none focus:text-gray-600"
                         aria-label="toggle menu">
                         <svg x-show="!isOpen" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none"
                             viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
@@ -45,19 +48,30 @@
 
             <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
             <div x-cloak :class="[isOpen ? 'translate-x-0 opacity-100 ' : 'opacity-0 -translate-x-full']"
-                class="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white dark:bg-gray-800 md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center">
+                class="absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white  md:mt-0 md:p-0 md:top-0 md:relative md:bg-transparent md:w-auto md:opacity-100 md:translate-x-0 md:flex md:items-center">
                 <div class="flex flex-col md:flex-row md:mx-6">
-                    <a href="index.php"
-                        class="font-sans my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-900 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
+                    <a class="my-2 text-gray-700 transition-colors duration-300 transform  hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
                         href="#">Home</a>
-                    <a href="update_page.php"
-                        class="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-900 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
-                        href="#">Archives</a>
-                    <a class="my-2 text-gray-700 transition-colors duration-300 transform dark:text-gray-900 hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
+                    <a class="my-2 text-gray-700 transition-colors duration-300 transform  hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
+                        href="#">Shop</a>
+                    <a class="my-2 text-gray-700 transition-colors duration-300 transform  hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
+                        href="#">Contact</a>
+                    <a class="my-2 text-gray-700 transition-colors duration-300 transform  hover:text-blue-500 dark:hover:text-blue-400 md:mx-4 md:my-0"
                         href="#">About</a>
                 </div>
 
+                <div class="flex justify-center md:block">
+                    <a class="relative text-gray-700 transition-colors duration-300 transform  hover:text-gray-600 dark:hover:text-gray-300"
+                        href="#">
+                        <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M3 3H5L5.4 5M7 13H17L21 5H5.4M7 13L5.4 5M7 13L4.70711 15.2929C4.07714 15.9229 4.52331 17 5.41421 17H17M17 17C15.8954 17 15 17.8954 15 19C15 20.1046 15.8954 21 17 21C18.1046 21 19 20.1046 19 19C19 17.8954 18.1046 17 17 17ZM9 19C9 20.1046 8.10457 21 7 21C5.89543 21 5 20.1046 5 19C5 17.8954 5.89543 17 7 17C8.10457 17 9 17.8954 9 19Z"
+                                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
 
+                        <span class="absolute top-0 left-0 p-1 text-xs text-white bg-blue-500 rounded-full"></span>
+                    </a>
+                </div>
             </div>
         </div>
     </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,php}"],
+  content: ["./src/**/*.{html,js,php}", "./"],
   theme: {
-    extend: {},
-    fontFamily: {
-      sans: [
-        "ui-sans-serif",
-        "system-ui",
-        "sans-serif",
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-        '"Noto Color Emoji"',
-      ],
-      montserrat: ["Montserrat"],
-      lato: ["Lato"],
-      garamond: ["Garamond"],
+    extend: {
+      colors: {
+        customcolor: "#23242b",
+      },
+
+      fontFamily: {
+        sans: [
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          '"Apple Color Emoji"',
+          '"Segoe UI Emoji"',
+          '"Segoe UI Symbol"',
+          '"Noto Color Emoji"',
+        ],
+        monts: ["'Montserrat', sans-serif"],
+        inter: ["'Inter', sans-serif"],
+      },
     },
   },
   plugins: [],
