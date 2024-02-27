41ab45ac295e8536dbf6d81459b76f73f094f3b1
r.montebon.536963@umindanao.edu.ph <ranielmontebon24@gmail.com>
2024-02-28 00:50:23 +0800

    I added more input fields to the add student page

--- addStudentPageDecoy.php
+++ addStudentPageDecoy.php
@@ -4,7 +4,7 @@ include("partials/nav.php");
 include("metalabsdb.php");
 ?>
 
-<form>
+<form action="insert_data.php" method="post">
     <div
         class="space-y-12 border p-10 sm:rounded-[24px] sm:my-10 sm:mx-10 lg:mx-36 lg:my-10 md:p-8 lg:p-10 md:m-10 shadow-md rounded-[0] md:rounded-[24px] lg:rounded-[24px]">
         <div class=" pb-12">
@@ -19,8 +19,8 @@ include("metalabsdb.php");
                         <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">First
                             name</label>
                         <div class="mt-2">
-                            <input type="text" name="first-name" id="first-name" autocomplete="given-name"
-                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6">
+                            <input type="text" name="fName" id="first-name" autocomplete="given-name"
+                                class="px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6">
                         </div>
                     </div>
 
@@ -28,8 +28,17 @@ include("metalabsdb.php");
                         <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Last
                             name</label>
                         <div class="mt-2">
-                            <input type="text" name="last-name" id="last-name" autocomplete="family-name"
-                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6">
+                            <input type="text" name="lName" id="last-name" autocomplete="family-name"
+                                class="px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6">
+                        </div>
+                    </div>
+
+                    <div class="sm:col-span-3">
+                        <label for="last-name" class="block text-sm font-medium leading-6 text-gray-900">Birth
+                            Date</label>
+                        <div class="mt-2">
+                            <input type="date" name="birthDate" id="birthDate"
+                                class="px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6">
                         </div>
                     </div>
 
@@ -37,29 +46,31 @@ include("metalabsdb.php");
                         <label for="contact-num" class="block text-sm font-medium leading-6 text-gray-900">Contact
                             Number</label>
                         <div class="mt-2">
-                            <input id="contact-num" name="contact-num" type="number"
-                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6">
+                            <input id="contact-num" name="contactNum" type="number"
+                                class="px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6">
                         </div>
                     </div>
 
-                    <!-- <div class="sm:col-span-3">
-                        <label for="country" class="block text-sm font-medium leading-6 text-gray-900">Country</label>
+                    <div class="sm:col-span-3">
+                        <label for="country" class="block text-sm font-medium leading-6 text-gray-900">Gender</label>
                         <div class="mt-2">
-                            <select id="country" name="country" autocomplete="country-name"
-                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset  sm:max-w-xs sm:text-sm sm:leading-6">
-                                <option>United States</option>
-                                <option>Canada</option>
-                                <option>Mexico</option>
+                            <select id="gender" name="gender"
+                                class="px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset  sm:max-w-xs sm:text-sm sm:leading-6">
+                                <option>Male</option>
+                                <option>Female</option>
+                                <option>Others</option>
                             </select>
                         </div>
-                    </div> -->
+                    </div>
+
+
 
                     <div class="col-span-full">
                         <label for="street-address" class="block text-sm font-medium leading-6 text-gray-900">Street
                             address</label>
                         <div class="mt-2">
                             <input type="text" name="street-address" id="street-address" autocomplete="street-address"
-                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset  sm:text-sm sm:leading-6">
+                                class="px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset  sm:text-sm sm:leading-6">
                         </div>
                     </div>
 
@@ -67,7 +78,7 @@ include("metalabsdb.php");
                         <label for="city" class="block text-sm font-medium leading-6 text-gray-900">City</label>
                         <div class="mt-2">
                             <input type="text" name="city" id="city" autocomplete="address-level2"
-                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6">
+                                class="px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6">
                         </div>
                     </div>
 
@@ -76,7 +87,7 @@ include("metalabsdb.php");
                             Province</label>
                         <div class="mt-2">
                             <input type="text" name="region" id="region" autocomplete="address-level1"
-                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6">
+                                class="px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6">
                         </div>
                     </div>
 
@@ -85,18 +96,87 @@ include("metalabsdb.php");
                             code</label>
                         <div class="mt-2">
                             <input type="number" name="postal-code" id="postal-code" autocomplete="postal-code"
-                                class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6">
+                                class="px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6">
                         </div>
                     </div>
                 </div>
+
+            </div>
+
+            <div class="pt-10 border-b border-gray-900/10 pb-12">
+                <h2 class="text-lg font-semibold leading-7 text-gray-900">Course</h2>
+                <p class="mt-1 text-sm leading-6 text-gray-600">Choose the department, college course, and year level of
+                    the student.</p>
+
+
+                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+
+
+                    <div class="sm:col-span-2">
+                        <label for="department"
+                            class="block text-sm font-medium leading-6 text-gray-900">Deparment</label>
+                        <div class="mt-2">
+                            <select id="department" name="department"
+                                class="px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset  sm:max-w-xs sm:text-sm sm:leading-6">
+                                <option>Select Department</option>
+                                <option>College of Engineering Education</option>
+                                <option>College of Computing Education</option>
+                                <option>College of Arts and Science Education</option>
+                                <option>College of Health Sciences Education</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="sm:col-span-2">
+                        <label for="courses" class="block text-sm font-medium leading-6 text-gray-900">Courses</label>
+                        <div class="mt-2">
+                            <select id="courses" name="courses"
+                                class="px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset  sm:max-w-xs sm:text-sm sm:leading-6">
+                                <option>Select Course</option>
+                                <option>Course 1</option>
+                                <option>Course 2</option>
+                                <option>Course 3</option>
+                                <option>Course 4</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="sm:col-span-2">
+                        <label for="yearLevel" class="block text-sm font-medium leading-6 text-gray-900">Year
+                            Level</label>
+                        <div class="mt-2">
+                            <select id="yearLevel" name="yearLevel"
+                                class="px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset  sm:max-w-xs sm:text-sm sm:leading-6">
+                                <option>Select Year Level</option>
+                                <option>1st Year</option>
+                                <option>2nd Year</option>
+                                <option>3rd Year</option>
+                                <option>4th Year</option>
+                                <option>5th Year</option>
+                                <option>Beyond</option>
+                            </select>
+                        </div>
+                    </div>
+
+                </div>
+
+
             </div>
         </div>
 
-        <div class="mt-6 flex items-center justify-end gap-x-6">
-            <button href="index.php" type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
-            <button type="submit"
-                class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Add
-                Student</button>
+
+
+        <div class="mt-6 flex items-center justify-end gap-x-4">
+            <a href="index.php" action="index.php"
+                class="border rounded-md px-7 py-1 border-gray-400 text-sm font-semibold leading-6 text-gray-900">Cancel</a>
+
+            <i>
+
+
+            </i>
+            <input value="Add Student" type="submit" name="addStudents"
+                class="cursor-pointer rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600" />
+
         </div>
 </form>
 
