a79fefc59b59daef458316fd3270492d2e341dc4
r.montebon.536963@umindanao.edu.ph <ranielmontebon24@gmail.com>
2024-03-01 16:56:11 +0800

    The edit page is partially complete

--- editStudent.php
+++ editStudent.php
@@ -2,30 +2,21 @@
 include("partials/head.php");
 include("partials/nav.php");
 include("metalabsdb.php");
-?>
 
-<?php
 if (isset($_GET['id'])) {
     $id = $_GET['id'];
     $query = "SELECT * FROM `student_enrollment` WHERE `id` = '$id'";
     $result = mysqli_query($connection, $query);
 
     if (!$result) {
-        die("query failed" . mysqli_error());
+        die("Query failed: " . mysqli_error($connection));
     } else {
         $row = mysqli_fetch_assoc($result);
     }
 }
 
-?>
-
-<?php
-if (isset($_POST['update_students'])) {
-
-    if(isset($_GET['id_new'])) {
-        $idnew = $_GET['id_new'];
-    }
-
+if (isset($_POST['updateStudent'])) {
+    $idnew = $_GET['id']; // Using the same ID for updating
     $fname = $_POST['fName'];
     $lname = $_POST['lName'];
     $birthdate = $_POST['birthDate'];
@@ -39,15 +30,16 @@ if (isset($_POST['update_students'])) {
     $courses = $_POST['courses'];
     $yearLevel = $_POST['yearLevel'];
 
-    $query =
-        "UPDATE `student_enrollment` SET `first_name` = '$fname', `last_name` = '$lname', 
+    $query = "UPDATE `student_enrollment` SET `first_name` = '$fname', `last_name` = '$lname', 
         `birthdate` = '$birthdate', `contact_number` = '$contactNum' WHERE `id` = '$idnew'";
+    
     $result = mysqli_query($connection, $query);
     
     if (!$result) {
-        die("query failed" . mysqli_error());
+        die("Query failed: " . mysqli_error($connection));
     } else {
-        header('location: index.php?update_msg=You have successfully updated the student data.');
+        header('Location: index.php?update_msg=You have successfully updated the student data.');
+        exit();
     }
 }
 ?>
@@ -111,6 +103,16 @@ window.onload = function() {
                     in this section.</p>
 
                 <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+
+                    <div class="sm:col-span-4">
+                        <label for="id" class="block text-sm font-medium leading-6 text-gray-900">Student
+                            ID</label>
+                        <div class="mt-2">
+                            <input disabled type="text" name="id" id="id" autocomplete="id" value="<?= $row['id'] ?>"
+                                class="px-5 block w-40 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6">
+                        </div>
+                    </div>
+
                     <div class="sm:col-span-3">
                         <label for="first-name" class="block text-sm font-medium leading-6 text-gray-900">First
                             name</label>
